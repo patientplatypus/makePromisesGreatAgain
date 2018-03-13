2657a6383e097a8e74703ac66cdcb765e014f4e9
Peter Weyand <pweyand@gmail.com>
2018-03-13 15:49:53 -0500

    push

--- truffle.js
+++ truffle.js
@@ -2,9 +2,9 @@ module.exports = {
   networks: {
     development: {
       host: "localhost",
-      port: 7545, // This is the Ganache default port. You can change it to the conventional 8545 if your network runs on 8545
+      port: 8545, // This is the Ganache default port. You can change it to the conventional 8545 if your network runs on 8545
       network_id: "*", // Match any network id. You may need to replace * with your network Id
-      from: "", // Add your unlocked account within the double quotes
+      from: "6e3e95f0c7044bc890ad921f4511a976a6d805dda081c39d267873b44fdb1e77", // Add your unlocked account within the double quotes
       gas: 4444444
     }
   }
